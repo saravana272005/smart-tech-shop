cc7493f874638a6d89c479d0c9c5ccd48d0dba9f
saravana272005 <saravana272005@gmail.com>
2025-09-30 15:15:50 +0530

    1.20

--- .env.js
+++ .env.js
@@ -1,10 +1,18 @@
 # .env file content (Check for spacing!)
-PORT=5000
-DB_HOST=localhost
-DB_USER=root
-DB_PASSWORD=Saravana@272005
+PORT=4000
+DB_HOST=gateway01.ap-southeast-1.prod.aws.tidbcloud.com
+DB_USER=WDR6ih12HhyZUUM.root
+DB_PASSWORD=iIjq1fk7UpyUBWjM
 DB_NAME=smartdb
 MAIL_USER=sts.smarttechshop2025@gmail.com
 MAIL_PASS=klqf bctp emuj tmtv
 RAZORPAY_KEY_ID=rzp_test_RH4lIyt42awGG1
 RAZORPAY_KEY_SECRET=igS5C3h1Oiuv7TK1E2gB3wLg
+
+
+
+# PORT=5000
+# DB_HOST=localhost
+# DB_USER=root
+# DB_PASSWORD=Saravana@272005
+# DB_NAME=smartdb
